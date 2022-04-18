43d45821a3d2d5e01d312eddc8c7dcd31b2cdb5d
Sean Callahan <s.callahan321@gmail.com>
2022-04-18 14:25:01 -0600

    test

--- baseball.py
+++ baseball.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 
-    # data = statcast('2022-04-01', '2022-04-18')
-    # test = {}
-    # test['value'] = data['pitch_type'].iloc[0]
+    data = statcast('2022-04-01', '2022-04-18')
+    test = {}
+    test['value'] = data['pitch_type'].iloc[0]
 
-    return ("Hi sean")
+    return test
